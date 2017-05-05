618d413c596ab7dfed1e52a2804db9d43668639f
Jessica Gilligan <jessicagilligan@Jessicas-MacBook-Pro.local>
2017-05-05 13:46:40 -0400

    update to connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  console.log(JAWSDB_URL);
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
