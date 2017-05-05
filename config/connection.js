813798136c3fdcc69c6d72b5cc90bc969247808c
Jessica Gilligan <jessicagilligan@Jessicas-MacBook-Pro.local>
2017-05-05 14:00:56 -0400

    testing direct connection to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
+var connection = process.env.JAWSDB_URL;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "entropy04",
-  database: "burgers_db"
-});
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "entropy04",
+//   database: "burgers_db"
+// });
+// }
 
 // Make connection.
 connection.connect(function(err) {
